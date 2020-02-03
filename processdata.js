be91c9f74a326c057b8724559d9d371fad04038e
Hugo <hugobrancowb@gmail.com>
2020-02-02 22:40:17 -0300

    fixed a problem in script processdata where data were being stored mult
    multiple times

--- processdata.js
+++ processdata.js
@@ -12,8 +12,8 @@ jsonContent.forEach(el => {
     entry2.month = new Date(entry2.month);
     // console.log(typeof entry2.total);
 
-    let i = lista_month.findIndex( obj => (obj.uf == entry1.uf) && (obj.month == entry1.month) && (obj.month == entry1.month));
-    let j = lista_year.findIndex( obj => (obj.uf == entry2.uf) && (obj.month.getYear() == entry2.month.getYear()));
+    let i = lista_month.findIndex( obj => (obj.uf == entry1.uf) && (obj.month.getYear() == entry1.month.getYear()) && (obj.month.getMonth() == entry1.month.getMonth()));
+    let j = lista_year.findIndex( obj => (obj.uf == entry2.uf) && (obj.year == entry2.month.getYear() + 1900));
     
     /* select and sum same month-year and state */
     if (i == -1) {
@@ -26,15 +26,15 @@ jsonContent.forEach(el => {
     /* select and sum same year and state */
     if (j == -1) {
         delete entry2.city; // won't be using city in this context. might as well delete it.
-        entry2.month = new Date(entry2.month.getYear() + 1900, 0);
+        entry2.year = entry2.month.getYear() + 1900;
+        delete entry2.month;
         lista_year.push(entry2);
     } else {
         lista_year[j].total += entry2.total;
     }
-
-    save_json_file(lista_month, 'data_per_state_month');
-    save_json_file(lista_year, 'data_per_state_year');
 });
+save_json_file(lista_month, 'data_per_state_month');
+save_json_file(lista_year, 'data_per_state_year');
 
 /* same function as in bolsafamilia.js */
 function save_json_file(list, name) {
