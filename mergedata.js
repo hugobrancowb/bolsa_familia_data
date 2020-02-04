bb80c5affa3e7f06db55e486cacf508893f4f005
Hugo <hugobrancowb@gmail.com>
2020-02-04 00:22:26 -0300

    mergedata doesnt delete repeated data anymore
    fixed an error where data where being mined more than once so now we do not need to check for duplicates anymore.
    numbers were checked and they match.

--- mergedata.js
+++ mergedata.js
@@ -15,29 +15,12 @@ fs.readdirSync(dir).forEach(arq => {
 files.map(el => {
     var contents = fs.readFileSync(dir + el);
     var jsonContent = JSON.parse(contents); // there's 15 items in this object
-    
-    /* check if entry is already inside vector to avoid known problems with mining */
-    jsonContent.map(entry => {
-        if (alldata.length > 0) {
-            var flag = 0; // if true, this is new data
-
-            alldata.map(data => {
-                if ((entry.city == data.city) && (entry.uf == data.uf) && (entry.total == data.total) && (entry.year == data.year)) {
-                    flag += 1;
-                }
-            });
-            
-            if(flag == 0) {
-                alldata.push(entry);
-            }
-        } else {
-            alldata.push(entry);
-        }
-    });    
+    alldata.push(...jsonContent);
+    count += jsonContent.length;
 });
-save_json_file(alldata, '../alldata')
+
+save_json_file(alldata, '../alldata');
 console.log('itens: ' + count);
-console.log('repetidos: ' + repeat);
 
 /* Functions */
 function save_json_file(list, name) {
