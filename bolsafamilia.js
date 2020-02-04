e2004c0f236c8fb7c9b7eb9f3dd53a716b600413
Hugo <hugobrancowb@gmail.com>
2020-02-03 23:52:23 -0300

    saves data by each year

--- bolsafamilia.js
+++ bolsafamilia.js
@@ -1,12 +1,10 @@
 const puppeteer = require('puppeteer');
-const puppeteerExtra = require('puppeteer-extra');
-const pluginStealth = require('puppeteer-extra-plugin-stealth');
 const $ = require('cheerio');
 
 /* Objects */
-function Entry_data(month, uf, city, total) {
+function Entry_data(year, uf, city, total) {
     // assign values for object
-    this.month = month;
+    this.year = year;
     this.uf = uf;
     this.city = city;
     this.total = total;
@@ -15,15 +13,14 @@ function Entry_data(month, uf, city, total) {
 /* Functions */
 async function main() {
     var list = [];
-    var year = 2018;
-    var month = ['02', '03']; // all months which data will be gathered from
+    var year = ['2019']; // all years which data will be gathered from
     
-    for (let i = 0; i < month.length; i++) {
-        console.log('Mês: '+ month[i]); // pode apagar
-        console.time('tempo total: ' + month[i]+'_'+year);
-        list_temp = await get_from_date(month[i], year); // returns a list of entries from the pages
-        console.timeEnd('tempo total: ' + month[i] + '_' + year);
-        list.push(...list_temp);        
+    for (let i = 0; i < year.length; i++) {
+        console.log('Ano: '+ year[i]); // pode apagar
+        console.time('tempo total: ' + '_' + year[i]);
+        list_temp = await get_from_date(year[i]); // returns a list of entries from the pages
+        console.timeEnd('tempo total: ' + '_' + year[i]);
+        list.push(...list_temp);
     }
     
     console.log('tamanho total: ' + list.length); // pode apagar
@@ -40,15 +37,18 @@ function save_json_file(list, name) {
     });
 }
 
-async function get_from_date(month, year) {
-    const url = 'http://www.portaldatransparencia.gov.br/beneficios/consulta?paginacaoSimples=true&tamanhoPagina=&offset=&direcaoOrdenacao=asc&de=01%2F' + month + '%2F' + year + '&ate=28%2F' + month + '%2F' + year + '&tipoBeneficio=1&colunasSelecionadas=linkDetalhamento%2ClinguagemCidada%2CmesAno%2Cuf%2Cmunicipio%2Cvalor&ordenarPor=mesAno&direcao=desc';
+async function get_from_date(year) {
+    const url = 'http://www.portaldatransparencia.gov.br/beneficios/consulta?paginacaoSimples=true&tamanhoPagina=&offset=&direcaoOrdenacao=asc&de=01%2F01%2F' + year + '&ate=31%2F12%2F' + year + '&tipoBeneficio=1&colunasSelecionadas=linkDetalhamento%2ClinguagemCidada%2CmesAno%2Cuf%2Cmunicipio%2Cvalor&ordenarPor=mesAno&direcao=desc';
+    // const url = 'http://www.portaldatransparencia.gov.br/beneficios/consulta?paginacaoSimples=true&tamanhoPagina=&offset=&direcaoOrdenacao=asc&de=01%2F01%2F2015&ate=31%2F12%2F2015&tipoBeneficio=1&colunasSelecionadas=linkDetalhamento%2ClinguagemCidada%2CmesAno%2Cuf%2Cmunicipio%2Cvalor&ordenarPor=mesAno&direcao=desc';
 
-    puppeteerExtra.use(pluginStealth()); // incognito mode
-    const browser = await puppeteerExtra.launch({
+    const browser = await puppeteer.launch({
         slowMo: 10, // delays requests, preventing host to block puppeteer
         headless: false // shows browser window
-    });    
+    });
+
     const page = await browser.newPage();
+    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3419.0 Safari/537.36');
+
     try { 
         await page.goto(url, {waitUntil: 'load', timeout: 90000});
     } catch (error) {
@@ -60,22 +60,23 @@ async function get_from_date(month, year) {
     var list = [];
     
     while (true) {
-        await page.waitFor(2000);
+        await page.waitFor(3000);
+        const next_button = '#lista_paginate ul.pagination > li:nth-child(2)';
+        await page.waitForSelector(next_button);
+
         var html = await page.content();
         var list_temp = await scrape_page(html);
         list.push(...list_temp);
         
-        let filename = year + '_' + month + '_' + 'page' + pages;
+        let filename = year + '_' + 'page' + pages;
         save_json_file(list_temp, filename);
-        pages += 1;
         
         console.log(pages + ' - ' + list.length); // pode apagar
+        pages += 1;
 
-        const next_button = '#lista_paginate ul.pagination > li:nth-child(2)';
-        await page.waitForSelector(next_button + ' a');
         const next_flag = await $(next_button, html).hasClass('disabled');
        
-        if ((!next_flag) && (pages < 2)) {
+        if ((!next_flag) && (pages < 5000)) {
             try {
                 await page.click(next_button + ' a');
             } catch (error) {
@@ -89,7 +90,7 @@ async function get_from_date(month, year) {
 }
 
 async function scrape_page(html) {
-    const month = await $('#lista tbody tr td.coluna-livre:nth-child(3) span', html);
+    const year = await $('#lista tbody tr td.coluna-livre:nth-child(3) span', html);
     const state = await $('#lista tbody tr td.coluna-livre:nth-child(4) span', html);
     const city = await $('#lista tbody tr td.coluna-livre:nth-child(5) span', html);
     const total = await $('#lista tbody tr td.coluna-livre:nth-child(6) span', html);
@@ -97,17 +98,17 @@ async function scrape_page(html) {
     lista = []
     
     for (let i = 0; i < state.length; i++) {
-        m_temp = $(month[i]).text();
-        m_temp = m_temp.split("/");        
+        y_temp = $(year[i]).text();
+        y_temp = y_temp.split("/");        
         t_temp = $(total[i]).text();
         t_temp = t_temp.substring(0, t_temp.length - 3).replace(".", "");
         
-        const m = new Date(m_temp[1], m_temp[0] - 1);
+        const y = y_temp[1];
         const s = $(state[i]).text();
         const c = $(city[i]).text();
         const t = parseFloat(t_temp);
 
-        lista.push(new Entry_data(m, s, c, t));
+        lista.push(new Entry_data(y, s, c, t));
     }
     
     return lista;
