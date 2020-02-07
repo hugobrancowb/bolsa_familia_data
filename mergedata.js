0f129e03f3e8d04020ab512ba000af68184b31d4
Hugo <hugobrancowb@gmail.com>
2020-02-07 00:47:55 -0300

    no duplicates (once again)
    since we're mining correctly, there's no need to delete duplicates anymore ()there are no duplicates)

--- mergedata.js
+++ mergedata.js
@@ -5,8 +5,6 @@ var fs = require("fs");
 const dir = "data/raw/";
 var files = []; // name of the files inside dir
 var alldata = []; // all data gathered
-var count = 0;
-var repeat = 0;
 
 fs.readdirSync(dir).forEach(arq => {
     files.push(arq);
@@ -17,31 +15,11 @@ files.map(el => {
     var jsonContent = JSON.parse(contents); // there's 15 or 50 items in this object
 
     jsonContent.map(entry => {
-        if (alldata.length > 0) {
-            var flag = 0; // if true, this is new data
-
-            alldata.map(data => {
-                if ((entry.city == data.city) && (entry.uf == data.uf) && (entry.total == data.total) && (entry.year == data.year)) {
-                    flag += 1;
-                }
-            });
-            
-            if(flag == 0) {
-                alldata.push(entry);
-                count += 1;
-            } else {
-                repeat += 1;
-            }
-        } else {
-            alldata.push(entry);
-            count += 1;
-        }
+        alldata.push(entry);
     });
 });
 
 save_json_file(alldata, 'alldata');
-console.log('itens: ' + count);
-console.log('repetidos: ' + repeat);
 
 /* Functions */
 function save_json_file(list, name) {
